2c4d5896e459d702c2696375cac36be0e31be660
fawwazer <mn,fawwaz99@gmail.com>
2024-03-16 12:16:53 +0700

    feat:create : services.go

--- features/user/entity.go
+++ features/user/entity.go
@@ -1,6 +1,9 @@
 package user
 
-import "github.com/labstack/echo"
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo"
+)
 
 type UserController interface {
 	Add() echo.HandlerFunc
@@ -8,13 +11,26 @@ type UserController interface {
 	Profile() echo.HandlerFunc
 }
 
+type UserService interface {
+	Register(newData User) error
+	Login(loginData User) (User, string, error)
+	Profile(token *jwt.Token) (User, error)
+}
+
+type UserModel interface {
+	InsertUser(newData User) error
+	UpdateUser(email string, data User) error
+	Login(email string) (User, error)
+	GetUserByEmail(email string) (User, error)
+}
+
 type User struct {
 	Nama      string
 	Email     string
 	Password  string
 	Tgl_lahir string
-	gender    string
-	alamat    string
+	Gender    string
+	Alamat    string
 }
 
 type Login struct {
@@ -26,7 +42,7 @@ type Register struct {
 	Nama      string `validate:"required,alpha"`
 	Email     string `validate:"required,email"`
 	Password  string `validate:"required,alphanum,min=8"`
-	Tgl_lahir string
-	gender    string
-	alamat    string
+	Tgl_lahir string `validate:"required,date"`
+	Gender    string `validate:"required,gender"`
+	Alamat    string `validate:"required,alamat"`
 }
